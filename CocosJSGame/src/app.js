859a63c488322dc8d86ad3cbd2ba6ad35524ab8e
MoffApps <tony.moffatt2@hotmail.com>
2014-11-12 17:32:11 +0000

    Week1

--- CocosJSGame/src/app.js
+++ CocosJSGame/src/app.js
@@ -11,59 +11,13 @@ var HelloWorldLayer = cc.Layer.extend({
         //    you may modify it.
         // ask the window size
         var size = cc.winSize;
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5,0.5));
+        sprite.setPosition(cc.p(size.width/2, size.height/2));
+        this.addChild(sprite,0);
 
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = new cc.MenuItemImage(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
-
-        var menu = new cc.Menu(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
-
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        helloLabel.runAction(
-            cc.spawn(
-                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-                cc.tintTo(2.5,255,125,0)
-            )
-        );
+        var sprite_Action = cc.MoveTo.create(2,cc.p(100,100));
+        sprite.runAction(sprite_Action);
         return true;
     }
 });
--- README.md
+++ README.md
@@ -2,3 +2,14 @@ codebros
 ========
 
 CBS Codebros
+
+
+Tutorial Series on Cocos2d-js
+https://www.youtube.com/playlist?list=PLRtjMdoYXLf7n9bghH1k63kisb-VDzGu1
+
+
+Tutorials 1-7 cover installing.
+
+Week 1
+
+Starting at tutorial 7 (Adding a sprite)
