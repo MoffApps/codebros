da9d80bc369e94b35d3ea5764fd7374aa8fec324
MoffApps <tony.moffatt2@hotmail.com>
2014-12-03 09:19:20 +0000

    End of class 3

--- CocosJSGame/src/app.js
+++ CocosJSGame/src/app.js
@@ -18,21 +18,21 @@ var HelloWorldLayer = cc.Layer.extend({
         ninja.setAnchorPoint(cc.p(0.5,0.5));
         ninja.setPosition(cc.p(size.width/10, size.height/2));
         this.addChild(ninja,0);
-        
-        this.schedule(this.gameLogic, 3);
-        
+ 
+        this.schedule(this.gameLogic, 1);   
         return true;
     },
+
     addMonster:function() {
+        var Y = Math.floor(Math.random() * size.height) + 1; 
     	var monster = new cc.Sprite.create(res.monster);
-    	var Y = 300; 
-    	
+        monster.setAnchorPoint(cc.p(0.5,0.5));	
     	monster.setPosition(size.width + 100, Y);   	
-    	this.addChild(monster); // 
-    	var actionMove = cc.MoveTo.create(2, cc.p(-100, Y)); //   	  	
+    	this.addChild(monster); 
+
+    	var actionMove = cc.MoveTo.create(1, cc.p(-100, Y)); //   	  	
     	monster.runAction(actionMove); 	
     },
-    
     gameLogic:function(dt) {
     	this.addMonster();
     }
